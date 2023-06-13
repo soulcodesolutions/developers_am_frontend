a2371a9c6fb4f37d0841696e7dbec9d163318566
Jyothis P <jyothisp52@gmail.com>
2023-06-13 09:31:22 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `devam-gatsby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
