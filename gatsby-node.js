7612ddaa15e69eef639c12a42d51c4f22aa6ebd5
Jyothis P <jyothisp52@gmail.com>
2023-07-30 12:49:31 +0530

    typedefs for no-data-builds

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,13 @@
 const path = require("path")
+const { strapiTypeDefs } = require("./strapiTypeDefs")
+
+exports.createSchemaCustomization = ({ actions }) => {
+    const { createTypes } = actions;
+    // Manually defining the Types instead of letting Gatbsy auto infer it from the recieved payload. 
+    // This helps to avoid crashes when we try to build without filling out everything on strapi.
+    // If you're making changes to your strapi schema, you might have to make changes here as well.
+    createTypes(strapiTypeDefs);
+}
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
     const { createPage } = actions
