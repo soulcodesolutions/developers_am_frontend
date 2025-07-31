5c5f5c77ac09197c12bb43f488d11d17d721a868
soulcodesolutions <soulcodesolutions@gmail.com>
2025-07-31 23:09:03 +0530

    new bg

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -21,7 +21,7 @@ export default function Hero({ data, data2, language }) {
         <>
             <div className="h-[20px]  sm:h-[80px] xl:h-0"></div>
 
-            <div className="bg-cover bg-no-repeat sm:bg-cover sm:bg-fixed    bg-[url(/images/bg13.png)] bg-top  sm:bg-top mx-auto h-[1200px] sm:h-[1000px] xl:h-[100vh] xl:min-h-[700px]  xl:max-h-[850px]  ">
+            <div className="bg-cover bg-no-repeat sm:bg-cover sm:bg-fixed    bg-[url(/images/bg14.png)] bg-top  sm:bg-top mx-auto h-[1200px] sm:h-[1000px] xl:h-[100vh] xl:min-h-[700px]  xl:max-h-[850px]  ">
                 <div className="h-full bg-black/40">
 
                     <div className="w-full h-full   flex flex-col items-center justify-end pt-[50px] max-h-[200px]   xl:pt-[20px] xl:max-h-[220px]  ">
